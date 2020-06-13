5aab59bc4362a0c1a23c6de2222669e93dc273a1
taruj khan <tarujkhan@DESKTOP-E8JDKB5.localdomain>
2020-06-13 09:53:00 -0500

    This read me lab is done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:index, :new, :create, :show]
+  get 'posts/index', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
+  post 'posts/create', to: 'posts#create'
+  get 'posts/show', to: 'posts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
